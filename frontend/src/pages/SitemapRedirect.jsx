29f97900736d72364bd92962f19d18aab0e2dc6c
soumboul fall <likadiop091955@gmail.com>
2025-06-15 23:59:32 +0100

    site

--- frontend/src/pages/SitemapRedirect.jsx
+++ frontend/src/pages/SitemapRedirect.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 
 const SitemapRedirect = () => {
     useEffect(()=>{
-        window.location.href = "https://bambaelectro-backend.onrender.com/sitemap.xml"
+        window.location.href = "https://sunubio-backend.onrender.com/sitemap.xml"
     },[])
   return (
     <p>Redirection vers le sitemap........</p>
