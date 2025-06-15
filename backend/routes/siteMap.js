ea5e6e00d01acde3e8c1454bd76c7300323d1fd1
soumboul fall <likadiop091955@gmail.com>
2025-06-16 00:38:09 +0100

    sitemap

--- backend/routes/siteMap.js
+++ backend/routes/siteMap.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import Product from '../models/productModel.js'; // adapte le chemin et l'extension
+import Product from '../models/productModel.js';
 
 const router = express.Router();
 
@@ -7,6 +7,7 @@ router.get('/sitemap.xml', async (req, res) => {
   try {
     const baseUrl = 'https://sunubio-frontend.onrender.com';
 
+    // Pages statiques à indexer
     const staticRoutes = [
       '/',
       '/collection',
@@ -17,27 +18,33 @@ router.get('/sitemap.xml', async (req, res) => {
       '/place-order',
       '/orders',
       '/profile'
+      // Ajoute ici d'autres pages publiques si besoin
     ];
 
     let urls = staticRoutes.map(route => `
-<url>
-  <loc>${baseUrl}${route}</loc>
-  <lastmod>${new Date().toISOString()}</lastmod>
-</url>`).join('');
+  <url>
+    <loc>${baseUrl}${route}</loc>
+    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+  </url>`).join('');
 
+    // Pages produits dynamiques
     const products = await Product.find({}, '_id updatedAt createdAt');
-
     products.forEach(product => {
       const lastMod = product.updatedAt || product.createdAt || Date.now();
       urls += `
-<url>
-  <loc>${baseUrl}/product/${product._id}</loc>
-  <lastmod>${new Date(lastMod).toISOString()}</lastmod>
-</url>`;
+  <url>
+    <loc>${baseUrl}/product/${product._id}</loc>
+    <lastmod>${new Date(lastMod).toISOString().split('T')[0]}</lastmod>
+  </url>`;
     });
 
+    // Génération du sitemap XML
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset 
+  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 
+                      http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
 ${urls}
 </urlset>`;
 
@@ -49,4 +56,5 @@ ${urls}
     res.status(500).send('Erreur lors de la génération du sitemap');
   }
 });
+
 export default router;
