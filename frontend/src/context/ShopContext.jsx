91542ff9cea4d4789839637055a00fa604057eb0
soumboul fall <likadiop091955@gmail.com>
2025-06-17 18:44:47 +0100

    shopcontext

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -8,7 +8,7 @@ export const ShopContext = createContext();
 const ShopContextProvider = (props) => {
   const currency = "F CFA";
   const delivery_fee = 2000;
-  const backendUrl = "https://sunubio-backend.onrender.com";
+  const backendUrl = import.meta.env.VITE_BACKEND_URL ||  "http://localhost:4000";
 
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
